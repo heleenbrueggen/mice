81e252a14b6dcd4af54da8a9d7ddb33b177ec733
heleenbrueggen <heleen355@gmail.com>
2024-01-19 14:34:14 +0100

    Create file for mbart imputation

---
+++ R/mice.impute.mbart.R
@@ -0,0 +1,15 @@
+#' A short description of your function
+#' 
+#' @name 
+#' @param x The first argument to be governed by the letter x.
+#' @return a short description of the expected return.
+#' @details 
+#' @note 
+#' @author 
+#' @references 
+#' @family
+#' @keywords 
+#' @export
+#' @examples
+#' examplecode 1
+#' examplecode 2
