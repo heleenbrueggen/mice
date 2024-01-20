4333c508727f4bbce4facd2ea33fa44749078a63
heleenbrueggen <heleen355@gmail.com>
2024-01-20 17:39:11 +0100

    Creating R file for mice.impute.bart

---
+++ R/mice.impute.bart.R
@@ -0,0 +1,19 @@
+#' A short description of your function
+#' 
+#' @name 
+#' @inheritParams mice.impute.pmm
+#' @return a short description of the expected return.
+#' @details 
+#' @note 
+#' @author 
+#' @references 
+#' @family
+#' @keywords 
+#' @export
+#' @examples
+#' examplecode 1
+#' examplecode 2
+
+mice.impute.bart <- function(y, ry, x, wy = NULL) {
+    
+}
